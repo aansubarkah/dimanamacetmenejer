02c2946ad01f099e31dd8119e104bb1f98e78a53
aansubarkah <aansubarkah@gmail.com>
2016-02-08 23:38:43 +0700

    add activities route

--- app/pods/activities/route.js
+++ app/pods/activities/route.js
@@ -1,17 +1,44 @@
+// to make JSHint happy
+/* global moment:false */
+/* global Hashids: false */
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+moment.locale('id');
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-    model: function(params) {
-        var arr_token = this.get('session.session.content.authenticated.token').split('.');
-        var userId = decodeURIComponent(escape(window.atob(arr_token[1])));
-        var arr_user = JSON.parse(userId);
-
+    model: function() {
+        var query = {};
         return Ember.RSVP.hash({
-            activity: this.store.find('activity', parseInt(arr_user['id']))
+            //activity: this.store.findAll('activity')
+            activity: this.store.query('activity', query)
         });
     },
     setupController: function(controller, model) {
         controller.set('activity', model.activity);
+        controller.set('total', model.activity.get('meta.total'));
+        controller.set('totalWeek', model.activity.get('meta.totalWeek'));
+
+        // data to display as chart
+        var headers = [];
+        var datum = [];
+        model.activity.forEach(function(item) {
+            //headers.push(item.get('name'));
+            headers.push(moment(item.get('name')).format('dddd, Do MMMM YYYY'));
+            datum.push(item.get('value'));
+        });
+        var dataForChart = {
+            labels: headers,
+            datasets: [{
+                label: "Daily Report",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: datum
+            }]
+        };
+        controller.set('dataForChart', dataForChart);
     }
 });
