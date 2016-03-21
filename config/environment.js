1ab6d39516743681d820c5101b5865a730e29d2c
aansubarkah <aansubarkah@gmail.com>
2016-03-21 21:46:20 +0700

    add visitor counter

--- config/environment.js
+++ config/environment.js
@@ -7,8 +7,8 @@ module.exports = function (environment) {
 		environment: environment,
 		baseURL: '/',
 		locationType: 'auto',
-        //apiURL: 'http://localhost:7654',
-        apiURL: 'http://apimenejer.macetdimana.com',
+        apiURL: 'http://localhost:7654',
+        //apiURL: 'http://apimenejer.macetdimana.com',
 		EmberENV: {
 			FEATURES: {
 				// Here you can enable experimental features on an ember canary build
@@ -51,8 +51,8 @@ module.exports = function (environment) {
 	};
 
 	ENV['ember-simple-auth-token'] = {
-		//serverTokenEndpoint: 'http://localhost:7654/users/token',// @todo change this on production server
-		serverTokenEndpoint: 'http://apimenejer.macetdimana.com/users/token',
+		serverTokenEndpoint: 'http://localhost:7654/users/token',// @todo change this on production server
+		//serverTokenEndpoint: 'http://apimenejer.macetdimana.com/users/token',
 		identificationField: 'username',
 		passwordField: 'password',
 		tokenPropertyName: 'token',
